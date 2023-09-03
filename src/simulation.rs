0c961358ace76d5448106218ec15186b170785d4
David Emmel <dgemmel2@gmail.com>
2023-09-03 13:05:00 +0800

    Clean up rule logic

--- src/simulation.rs
+++ src/simulation.rs
@@ -3,7 +3,7 @@ use image::{Rgb, RgbImage};
 use palette::Srgb;
 use rand::{seq::SliceRandom, Rng};
 
-use crate::grid::{Grid, GridEnumerator, GridLike};
+use crate::grid::{Grid, GridEnumerator, GridLike, GridWindow};
 
 pub struct State {
     elements: Grid<Tile>,
@@ -120,102 +120,10 @@ impl State {
     }
 
     fn intended_movements(&self) -> Grid<(isize, isize)> {
-        use Element::*;
-
-        let mut rng = rand::thread_rng();
-
         let intended_movements = self
             .elements
             .windows(3)
-            .map(|w| {
-                match (
-                    (
-                        w.get(-1, -1).map(|t| &t.element),
-                        w.get(0, -1).map(|t| &t.element),
-                        w.get(1, -1).map(|t| &t.element),
-                    ),
-                    (
-                        w.get(-1, 0).map(|t| &t.element),
-                        w.get(0, 0).map(|t| &t.element),
-                        w.get(1, 0).map(|t| &t.element),
-                    ),
-                    (
-                        w.get(-1, 1).map(|t| &t.element),
-                        w.get(0, 1).map(|t| &t.element),
-                        w.get(1, 1).map(|t| &t.element),
-                    ),
-                ) {
-                    // Air rises above soil and water
-                    ((_, Some(Water | Soil), _), (_, Some(Air), _), (_, _, _)) => (0, -1),
-                    // Soil falls below air and water
-                    (
-                        (_, _, _),
-                        (_, Some(Soil), _),
-                        (None | Some(Air | Water), Some(Water | Air), None | Some(Air | Water)),
-                    ) => (0, 1),
-                    // Soil rolls down hill
-                    (
-                        (_, _, _),
-                        (Some(Air | Water), Some(Soil), Some(Water | Air) | None),
-                        (Some(Air | Water), Some(Soil), Some(Soil) | None),
-                    ) => (-1, 1),
-                    (
-                        (_, _, _),
-                        (None | Some(Water | Air), Some(Soil), Some(Air | Water)),
-                        (None | Some(Soil), Some(Soil), Some(Air | Water)),
-                    ) => (1, 1),
-                    (
-                        (_, _, _),
-                        (Some(Air | Water), Some(Soil), Some(Air | Water)),
-                        (Some(Air | Water), Some(Soil), Some(Air | Water)),
-                    ) => (*[-1, 1].choose(&mut rng).unwrap(), 1),
-                    // Soil can form structure
-                    ((_, _, _), (_, Some(Soil), _), (Some(Soil), _, _)) => (0, 0),
-                    ((_, _, _), (_, Some(Soil), _), (_, _, Some(Soil))) => (0, 0),
-                    // Water rises above soil
-                    ((_, Some(Soil), _), (_, Some(Water), _), (_, Some(Soil | Water), _)) => {
-                        (0, -1)
-                    }
-                    // Water falls below air
-                    ((_, Some(Air | Water), _), (_, Some(Water), _), (_, Some(Air), _)) => (0, 1),
-                    // Waters rolls down hills
-                    (
-                        (_, _, _),
-                        (Some(Air), Some(Water), _),
-                        (Some(Air), Some(Soil | Water), Some(Soil | Water) | None),
-                    ) => (-1, 1),
-                    (
-                        (_, _, _),
-                        (_, Some(Water), Some(Air)),
-                        (None | Some(Soil | Water), Some(Soil | Water), Some(Air)),
-                    ) => (1, 1),
-                    (
-                        (_, _, _),
-                        (Some(Air), Some(Water), Some(Air)),
-                        (Some(Air), Some(Soil | Water), Some(Air)),
-                    ) => (*[-1, 1].choose(&mut rng).unwrap(), 1),
-                    // Water tries to flatten
-                    (
-                        (_, _, _),
-                        (Some(Air), Some(Water), None | Some(Water)),
-                        (Some(Soil | Water), Some(Soil | Water), Some(Soil | Water) | None),
-                    ) => (-1, 0),
-                    (
-                        (_, _, _),
-                        (None | Some(Water), Some(Water), Some(Air)),
-                        (None | Some(Soil | Water), Some(Soil | Water), Some(Soil | Water)),
-                    ) => (1, 0),
-                    (
-                        (_, _, _),
-                        (Some(Air), Some(Water), Some(Air)),
-                        (Some(Water), Some(Soil | Water), Some(Water)),
-                    ) => (*[-1, 1].choose(&mut rng).unwrap(), 0),
-                    x => {
-                        // println!("Unhandled configuration: {x:?}");
-                        (0, 0)
-                    }
-                }
-            })
+            .map(|w| w.get(0, 0).unwrap().intended_movement(&w))
             .collect();
         let intended_movements = Grid::from_cells(
             self.elements.width(),
@@ -315,6 +223,111 @@ impl Tile {
             Element::Air => Srgb::new(221, 255, 247),
             Element::Soil => Srgb::new(169, 113, 75),
             Element::Water => Srgb::new(46, 134, 171),
+        }
+    }
+
+    fn intended_movement(&self, neighbors: &GridWindow<'_, Tile, Grid<Tile>>) -> (isize, isize) {
+        use Element::*;
+
+        let mut rng = rand::thread_rng();
+
+        let adjacent = (
+            (
+                neighbors.get(-1, -1).map(|t| &t.element),
+                neighbors.get(0, -1).map(|t| &t.element),
+                neighbors.get(1, -1).map(|t| &t.element),
+            ),
+            (
+                neighbors.get(-1, 0).map(|t| &t.element),
+                neighbors.get(1, 0).map(|t| &t.element),
+            ),
+            (
+                neighbors.get(-1, 1).map(|t| &t.element),
+                neighbors.get(0, 1).map(|t| &t.element),
+                neighbors.get(1, 1).map(|t| &t.element),
+            ),
+        );
+
+        match self.element {
+            Air => match adjacent {
+                // Air rises above soil and water
+                ((_, Some(Water | Soil), _), (_, _), (_, _, _)) => (0, -1),
+                x => {
+                    // println!("Unhandled configuration: {x:?}");
+                    (0, 0)
+                }
+            },
+            Soil => match adjacent {
+                // Soil falls below air and water
+                (
+                    (_, _, _),
+                    (_, _),
+                    (None | Some(Air | Water), Some(Water | Air), None | Some(Air | Water)),
+                ) => (0, 1),
+                // Soil rolls down hill
+                (
+                    (_, _, _),
+                    (Some(Air | Water), Some(Water | Air) | None),
+                    (Some(Air | Water), Some(Soil), Some(Soil) | None),
+                ) => (-1, 1),
+                (
+                    (_, _, _),
+                    (None | Some(Water | Air), Some(Air | Water)),
+                    (None | Some(Soil), Some(Soil), Some(Air | Water)),
+                ) => (1, 1),
+                (
+                    (_, _, _),
+                    (Some(Air | Water), Some(Air | Water)),
+                    (Some(Air | Water), Some(Soil), Some(Air | Water)),
+                ) => (*[-1, 1].choose(&mut rng).unwrap(), 1),
+                // Soil can form structure
+                ((_, _, _), (_, _), (Some(Soil), _, _)) => (0, 0),
+                ((_, _, _), (_, _), (_, _, Some(Soil))) => (0, 0),
+                x => {
+                    // println!("Unhandled configuration: {x:?}");
+                    (0, 0)
+                }
+            },
+            Water => match adjacent {
+                // Water rises above soil
+                ((_, Some(Soil), _), (_, _), (_, Some(Soil | Water), _)) => (0, -1),
+                // Water falls below air
+                ((_, Some(Air | Water), _), (_, _), (_, Some(Air), _)) => (0, 1),
+                // Waters rolls down hills
+                (
+                    (_, _, _),
+                    (Some(Air), _),
+                    (Some(Air), Some(Soil | Water), Some(Soil | Water) | None),
+                ) => (-1, 1),
+                (
+                    (_, _, _),
+                    (_, Some(Air)),
+                    (None | Some(Soil | Water), Some(Soil | Water), Some(Air)),
+                ) => (1, 1),
+                ((_, _, _), (Some(Air), Some(Air)), (Some(Air), Some(Soil | Water), Some(Air))) => {
+                    (*[-1, 1].choose(&mut rng).unwrap(), 1)
+                }
+                // Water tries to flatten
+                (
+                    (_, _, _),
+                    (Some(Air), None | Some(Water)),
+                    (Some(Soil | Water), Some(Soil | Water), Some(Soil | Water) | None),
+                ) => (-1, 0),
+                (
+                    (_, _, _),
+                    (None | Some(Water), Some(Air)),
+                    (None | Some(Soil | Water), Some(Soil | Water), Some(Soil | Water)),
+                ) => (1, 0),
+                (
+                    (_, _, _),
+                    (Some(Air), Some(Air)),
+                    (Some(Water), Some(Soil | Water), Some(Water)),
+                ) => (*[-1, 1].choose(&mut rng).unwrap(), 0),
+                x => {
+                    // println!("Unhandled configuration: {x:?}");
+                    (0, 0)
+                }
+            },
         }
     }
 }
